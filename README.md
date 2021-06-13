33f8e995739c60f5fc588820908edeb908e8dcba
Alex <probesalesman@gmail.com>
2021-06-13 16:03:27 -0400

    refactor to prompt 2, question 1

---
+++ README.md
@@ -0,0 +1,28 @@
+# Chronograph Case Study
+
+## Assumptions about database
+
+- asd
+- foo
+- bar
+
+---
+
+## Assumptions about data store
+
+- foo
+- bar
+- baz
+- `determineNumberPagesInReport` function was first attempt at prompt 2, question 1 - for each report, iterate through all documents and find all related to a given report and create a map of `{documentId: true}` for every `document` related to a report. Then iterate through all `pages`, and if a page is found in that `document` map, increment a variable to indicate we found a page related to a document
+  - problem is, for every report we'd iterate through all documents and pages once
+  - want to reduce the number of times we'd need to iterate through `documents` and `pages` data since I am assuming there'd be many documents associated with a report, and many more pages associated with a single document
+  - idea is to create a data structure by accessing `documents` and `pages` once, and then using that data structure to determine number of pages for a single report in constant time look up, or at least improve efficiency somewhat so we don't have to loop through all documents and all reports every time
+
+One note I have: regarding `prompt 2, question 1`, this seems like an operation more suited for the DB to handle since RDBMS are definitely better at these types of operations than JS, especially for larger data sets. JS better for handling IO tasks like making many db calls concurrently
+
+- from my perspective, if this `prompt 2, question 1` is being run on the front end of some application, we should let the db handle this operation if the size of the data set is large (as to what "large" might be, I am unsure at the moment) - might be faster to let the db handle this on the backend depending on size. I assume `prompt 2, question 3`, while not directly addressing `prompt 2, question 1` sort of acknowledges this by asking how we'd transform a seemingly heavy task using JS to a task that is delegated to an API call instead
+
+  - and if the data set isn't that large (what is considered "large" still not known) and we are doing this computation on the frontend, then the implementation for `prompt 2, question 1` might not matter so much from a performance perspective. Would want to wait to know that this operation is a performance bottle neck before trying to optimize it further, as the performance bottle neck on the front end may come from slow or unneccesary renders, or some other source.
+  - if trying to handle `prompt 2, question 1` on the back end in some `node/express` api or `rails`, the db still seems like the better choice to handle this type of computation
+
+- I have a similar intuition for `prompt 2, question 1` - if trying to perform a string search on millions of records using JS that could otherwise be handled by our db or some other data store, it'd be more efficient to let the db do this computation which relates to `prompt 2, question 3`
--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,93 @@ const store: IStore = {
   },
 };
 
+// iterate through all pages and make object where key is document id, and value is a set or array, whatever, that contains all pages for a document
+function compileReportsWithPageCounts() {
+  let docToPagesCountMap: { [id: number]: number } = {};
+  for (let pageId in store.page) {
+    let currPage = store.page[pageId];
+
+    if (docToPagesCountMap[currPage.document_id] === undefined) {
+      docToPagesCountMap[currPage.document_id] = 1;
+    } else {
+      docToPagesCountMap[currPage.document_id] += 1;
+    }
+  }
+
+  // iterate through all documents and make object where key is documentId and value is number of pages associated with document
+  // let docToPagesCount: { [docId: number]: number } = {};
+  let reportToDocsMap: {
+    [reportId: number]: { [documentId: number]: boolean };
+  } = {};
+  for (let docId in store.document) {
+    let currDoc = store.document[docId];
+
+    // fill out map of report to documents associated with it like: {4: {7: true, 5: true}}
+    if (reportToDocsMap[currDoc.report_id] === undefined) {
+      reportToDocsMap[currDoc.report_id] = { [currDoc.id]: true };
+    } else {
+      reportToDocsMap[currDoc.report_id][currDoc.id] = true;
+    }
+
+    // // map document id to number of pages associated with document
+    // if (docToPagesCountMap[currDoc.id] !== undefined) {
+    //   docToPagesCount[currDoc.id] = docToPagesCountMap[currDoc.id];
+    // }
+  }
+  // need map of all reports to each document associated with it, can generate this in above loop through documents
+
+  let reportsPagesCounts: { [reportId: number]: number } = {};
+  for (let reportId in store.report) {
+    let currReport = store.report[reportId];
+    // possible to have a report without documents, or documents without pages?
+    if (reportToDocsMap[currReport.id] !== undefined) {
+      // initialize the mapping from reportId to pageCount as 0 initially
+      reportsPagesCounts[currReport.id] = 0;
+      let reportToDocuments = Object.keys(reportToDocsMap[reportId]);
+
+      let pageCount = 0;
+      for (let documentId of reportToDocuments) {
+        if (docToPagesCountMap[Number(documentId)] !== undefined) {
+          pageCount += docToPagesCountMap[Number(documentId)];
+        }
+        reportsPagesCounts[currReport.id] = pageCount;
+      }
+
+      // possible to have a document with no pages according to store data, so need to make sure a document has pages before trying to add its page count
+    } else {
+      // if there isn't an entry for a report ID in reportToDocsMap, assume there are no documents associated with that report, and thus no pages
+      reportsPagesCounts[currReport.id] = 0;
+    }
+  }
+
+  console.log("docToPagesCountMap", docToPagesCountMap);
+  // console.log("docToPagesCount", docToPagesCount);
+  console.log("reportToDocsMap", reportToDocsMap);
+  console.log("reportsPagesCounts", reportsPagesCounts);
+  return reportsPagesCounts;
+}
+compileReportsWithPageCounts();
+
+// iterate through all reports, and for each reportId, need to look through all documents associated with that reportId, sum up count of pages associated with a
+// document and associate that value with each reportId
+// we are iterating through all reports, and all documents here
+
+// // create object "test" by iterating through documents
+// // for each document, check if the report_id of that document is in "test"
+// // if it isn't, create an entry in test where key is report_id and value is an object of structure {}
+
+// // maps report ids to document ids - maybe use a set to hold doc ids for each report
+// var reportsToDocs = {
+//   4: { 21: true, 37: true, 54: true },
+//   5: { 23: true, 3: true, 5: true },
+// };
+
+// // maps page ids to document ids
+// var pageToDocs = {
+//   1: 21,
+//   2: 21,
+// };
+
 // choosing to pass in the data source we would read from in case there are multiple data sources with the same structure as IStore
 function determineNumberPagesInReport(
   reportId: number,
@@ -64,6 +151,8 @@ function determineNumberPagesInReport(
   | string {
   // handle case where we are passed a reportId that doesn't exist - should also handle if type of reportId is not a number or cannot be
   // converted from string to number - not as necessary to handle second case in TS?
+
+  // if passed an invalid report, do we end the function early?
   const reportData = accessReportData(reportId, dataStore);
 
   if (typeof reportData === "string") {
@@ -76,7 +165,7 @@ function determineNumberPagesInReport(
       documentsMatching[documentId] = true;
     }
   }
-  console.log("documents matching", documentsMatching);
+  // console.log("documents matching", documentsMatching);
   let pagesMatching: number = 0;
   // for each page in the store, find which ones are related to every document we found
   for (let pageId in dataStore.page) {
